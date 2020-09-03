97834123c0fe32d0992dad46f96aff9ae101dd6d
Raymison <raymison1802@gmail.com>
2020-09-03 16:06:29 -0400

    request body

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,16 @@ const { response, request } = require('express');
 // aplicação criada
 const app = express();
 
+// receber parâmetros do tipo json
+app.use(express.json());
+
 // busca informações
 app.get('/projects', (request, response) => {
+    const { title, owner } = request.query;
+
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -14,6 +22,9 @@ app.get('/projects', (request, response) => {
 
 // envia informações
 app.post('/projects', (request, response) =>{
+    const body = request.body;
+    console.log(body);
+    
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -23,6 +34,9 @@ app.post('/projects', (request, response) =>{
 
 // edita informações
 app.put('/projects/:id', (request, response)=> {
+    const params = request.params;
+
+    console.log(params);
     return response.json([
         'Projeto 4',
         'Projeto 2',
