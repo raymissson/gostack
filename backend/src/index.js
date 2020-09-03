bbe68709569bb4f25d66866f16d58c884c92475e
Raymison <raymison1802@gmail.com>
2020-09-03 15:43:52 -0400

    testando rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,15 +4,39 @@ const { response, request } = require('express');
 // aplicação criada
 const app = express();
 
-// get recebe o primeiro parâmetro, o qual quer observar 
-// segundo parâmetro recebe uma função (arrow function)
+// busca informações
 app.get('/projects', (request, response) => {
-    return response.json({ message: 'Testando o back-end' });
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
 });
 
+// envia informações
 app.post('/projects', (request, response) =>{
-    return 'teste';
-})
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+// edita informações
+app.put('/projects/:id', (request, response)=> {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+// deletar informações
+app.delete('/projects/:id', (request, response)=> {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
 
 // porta para acessar url
 app.listen(3333, () => {
